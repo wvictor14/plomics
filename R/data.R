1a9b0becd396affdd5e5ef5cb6b9db5a00da0b80
Victor Yuan <victor.2wy@gmail.com>
2018-03-04 11:18:35 -0800

    updated data documentation

--- R/data.R
+++ R/data.R
@@ -10,5 +10,54 @@
 #'for biologically-relevant analysis of the Illumina Infinium
 #'HumanMethylation450 BeadChip array. Epigenetics & Chromatin, 6, 4.
 #' http://doi.org/10.1186/1756-8935-6-4
+#' 
+#' https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GPL16304
+"mprice_CH"
 
-"CH_mprice"
+#' Probes that are on both the 450K and EPIC arrays
+#'
+#' A character vector of 453152 probes names that are on both the 450k and EPIC 
+#'Illummina DNA methylation microarrays. All of the snp probes on the EPIC (59)
+#'are included - only 6 are missing from the 450k array. These names were taken
+#'from minfiData and minfiDataEPIC, because the illumina annotations do not have
+#'snp probe information.
+#'
+#'@format A character vector of probe names with length 453152
+#'@source minfiData 0.24.0, minfiDataEPIC 1.4.0
+"overlap_450k_EPIC"
+
+#' Probes identified as invariable in indepedent buccal datasets
+#' 
+#' A character vector of 120009 probe names identified as nonvariable between
+#' multiple BUCCAL datasets.
+#' 
+#' @format A character vector of probe names with length 120009
+#' @source Edgar R.D., Jones M.J., Robinson W.P., Kobor M.S. (2017) An 
+#' empirically driven data reduction method on the human 450K methylation array 
+#' to remove tissue specific non-variable CpGs. Clin. Epigenet ., 9, 11.
+#' https://github.com/redgar598/Tissue_Nonvariable_450K_CpGs
+"invar_Bu"
+
+#' Probes identified as invariable in indepedent blood datasets
+#' 
+#' A character vector of 114204 probe names identified as nonvariable between
+#' multiple BLOOD datasets.
+#' 
+#' @format A character vector of probe names with length 114204
+#' @source Edgar R.D., Jones M.J., Robinson W.P., Kobor M.S. (2017) An 
+#' empirically driven data reduction method on the human 450K methylation array 
+#' to remove tissue specific non-variable CpGs. Clin. Epigenet ., 9, 11.
+#' https://github.com/redgar598/Tissue_Nonvariable_450K_CpGs
+"invar_Bl"
+
+#' Probes identified as invariable in indepedent placenta datasets
+#' 
+#' A character vector of 101367 probe names identified as nonvariable between
+#' multiple PLACENTA datasets.
+#' 
+#' @format A character vector of probe names with length 101367
+#' @source Edgar R.D., Jones M.J., Robinson W.P., Kobor M.S. (2017) An 
+#' empirically driven data reduction method on the human 450K methylation array 
+#' to remove tissue specific non-variable CpGs. Clin. Epigenet ., 9, 11.
+#' https://github.com/redgar598/Tissue_Nonvariable_450K_CpGs
+"invar_Pl"
